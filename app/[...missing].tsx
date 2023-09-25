dca9293b6a632ff219084bbd3cb730f9f5e810b8
André Felipe Guimarães <andreafg.jfg@gmail.com>
2023-09-25 10:53:43 -0300

    ESLint + Prettier in Project

--- app/[...missing].tsx
+++ app/[...missing].tsx
@@ -1,17 +1,21 @@
-import { Link } from 'expo-router';
+import { Link } from 'expo-router'
 
-import { Text, View } from '@/components/Themed';
-import tw from '@/lib/tailwind';
+import { Text, View } from '@/components/Themed'
+import tw from '@/lib/tailwind'
 
 export default function NotFoundScreen() {
   return (
     <View style={tw`flex-1 items-center justify-center`}>
       <Text style={tw`text-2xl font-bold`}>Oops!</Text>
-      <View style={tw`my-5 h-0.5 w-4/5`} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={tw`my-5 h-0.5 w-4/5`}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <Text style={tw`text-lg`}>Esta tela não existe.</Text>
       <Link href="/" style={tw`mt-5 py-5`}>
-        <Text style={tw`text-blue-500 text-base`}>Ir para a tela inicial!</Text>
+        <Text style={tw`text-base text-blue-500`}>Ir para a tela inicial!</Text>
       </Link>
     </View>
-  );
+  )
 }
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Platform } from 'react-native'
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
-import tw from '@/lib/tailwind';
-
+import EditScreenInfo from '@/components/EditScreenInfo'
+import { Text, View } from '@/components/Themed'
+import tw from '@/lib/tailwind'
 
 export default function ModalScreen() {
   return (
     <View style={tw`flex-1 items-center justify-center`}>
       <Text style={tw`text-2xl font-bold`}>Modal</Text>
-      <View style={tw`my-7 h-0.5 w-4/5`} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={tw`my-7 h-0.5 w-4/5`}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="app/modal.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
     </View>
-  );
+  )
 }
